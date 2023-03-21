254c2acc7d376852e38b2af991aa62264b3f387a
shake551 <hiroki.yamako@gmail.com>
2023-03-21 23:44:34 +0900

    fix format

--- lib/domain/model/your_color.dart
+++ lib/domain/model/your_color.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:what_color/domain/model/color_base.dart';
 
 part 'your_color.freezed.dart';
 
@@ -7,13 +8,13 @@ part 'your_color.g.dart';
 
 @freezed
 class YourColor with _$YourColor {
-  const factory YourColor(String id,
-      String comment,
-      double latitude,
-      double longitude,
-      int r,
-      int g,
-      int b,) = _YourColor;
+  const factory YourColor(
+    String id,
+    String comment,
+    double latitude,
+    double longitude,
+    ColorBase color
+  ) = _YourColor;
 
   factory YourColor.fromJson(Map<String, dynamic> json) =>
       _$YourColorFromJson(json);
@@ -34,11 +35,11 @@ class YourColor with _$YourColor {
   }
 
   static List<YourColor> fromFirestoreList(
-      List<QueryDocumentSnapshot> jsonList,) {
+    List<QueryDocumentSnapshot> jsonList,
+  ) {
     final yourColorList = <YourColor>[];
     for (final ele in jsonList) {
-      yourColorList
-          .add(YourColor.fromFirestore(ele));
+      yourColorList.add(YourColor.fromFirestore(ele));
     }
 
     return yourColorList;
