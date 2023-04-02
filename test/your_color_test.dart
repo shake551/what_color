6f58654680f7b29b57d0bd565fd924d2dff7a97e
shake551 <hiroki.yamako@gmail.com>
2023-04-02 22:14:38 +0900

    update your color model test

--- test/your_color_test.dart
+++ test/your_color_test.dart
@@ -7,10 +7,11 @@ import 'package:what_color/domain/model/your_color.dart';
 void main() {
   test('test fromJson', () {
     const jsonString =
-        '{"id":"hoge","comment":"come","latitude":100,"longitude":120,"color":{"r":120,"g":250,"b":175}}';
+        '{"id":"hoge","userId":"shake","comment":"come","latitude":100,"longitude":120,"color":{"r":120,"g":250,"b":175}}';
     final fromJsonYourColor =
         YourColor.fromJson(json.decode(jsonString) as Map<String, dynamic>);
-    const color = YourColor('hoge', 'come', 100, 120, ColorBase(120, 250, 175));
+    const color =
+        YourColor('hoge', 'shake', 'come', 100, 120, ColorBase(120, 250, 175));
 
     expect(fromJsonYourColor, color);
   });
@@ -18,15 +19,15 @@ void main() {
   test('test fromJsonList', () {
     const jsonList = '''
     [
-    {"id":"hoge","comment":"come","latitude":100,"longitude":120,"color":{"r":120,"g":250,"b":175}},
-    {"id":"fuga","comment":"come","latitude":100,"longitude":120,"color":{"r":120,"g":250,"b":175}}
+    {"id":"hoge","userId":"shake","comment":"come","latitude":100,"longitude":120,"color":{"r":120,"g":250,"b":175}},
+    {"id":"fuga","userId":"sanma","comment":"come","latitude":100,"longitude":120,"color":{"r":120,"g":250,"b":175}}
     ]
     ''';
     final fromJsonYourColor =
         YourColor.fromJsonList(json.decode(jsonList) as List<dynamic>);
     const colorList = [
-      YourColor('hoge', 'come', 100, 120, ColorBase(120, 250, 175)),
-      YourColor('fuga', 'come', 100, 120, ColorBase(120, 250, 175)),
+      YourColor('hoge', 'shake', 'come', 100, 120, ColorBase(120, 250, 175)),
+      YourColor('fuga', 'sanma', 'come', 100, 120, ColorBase(120, 250, 175)),
     ];
 
     expect(fromJsonYourColor, colorList);
