1ba8b08fd6909ba53ad1a9c17b91cba98247b525
shake551 <hiroki.yamako@gmail.com>
2023-04-02 22:15:08 +0900

    add your color list logic

--- lib/widget/your_color_list.dart
+++ lib/widget/your_color_list.dart
@@ -1,6 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:what_color/domain/model/color_base.dart';
 import 'package:what_color/domain/model/your_color.dart';
+import 'package:what_color/domain/repository/your_color_repository.dart';
 
 class YourColorList extends StatefulWidget {
   const YourColorList({super.key});
@@ -10,13 +11,29 @@ class YourColorList extends StatefulWidget {
 }
 
 class YourColorListState extends State<YourColorList> {
-  final feelList = [
-    const YourColor('hoge', 'test', 1, 1, ColorBase(120, 250, 250)),
-    const YourColor('hoge', 'test', 1, 1, ColorBase(120, 250, 250)),
-  ];
+  List<YourColor> feelList = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    setFeelList();
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (feelList.isEmpty) {
+      return Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              CircularProgressIndicator(),
+            ],
+          ),
+        ),
+      );
+    }
     return ListView.builder(
       padding: const EdgeInsets.all(8),
       itemCount: feelList.length,
@@ -32,5 +49,15 @@ class YourColorListState extends State<YourColorList> {
         );
       },
     );
+  }
+
+  Future<void> setFeelList() async {
+    final yourColor = await YourColorRepository.getYourColorByUserID(
+      FirebaseAuth.instance.currentUser!.uid,
+    );
+
+    setState(() {
+      feelList = yourColor;
+    });
   }
 }
