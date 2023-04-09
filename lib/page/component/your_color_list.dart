4d508edc0029736443b36df3670630f59de27dd6
shake551 <hiroki.yamako@gmail.com>
2023-04-09 22:42:28 +0900

    update your color list component

---
+++ lib/page/component/your_color_list.dart
@@ -0,0 +1,38 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:what_color/domain/model/your_color.dart';
+import 'package:what_color/domain/repository/your_color_repository.dart';
+import 'package:what_color/widget/your_color_list_widget.dart';
+
+class YourColorList extends StatefulWidget {
+  const YourColorList({super.key});
+
+  @override
+  State<StatefulWidget> createState() => YourColorListState();
+}
+
+class YourColorListState extends State<YourColorList> {
+  List<YourColor> feelList = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    setFeelList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return YourColorListWidget(feelList: feelList);
+  }
+
+  Future<void> setFeelList() async {
+    final yourColor = await YourColorRepository.getYourColorByUserID(
+      FirebaseAuth.instance.currentUser!.uid,
+    );
+
+    setState(() {
+      feelList = yourColor;
+    });
+  }
+}
